296057b85ce01e420ac3a3e469ecf44b3d06589e
Aditya Narla <narla.a@northeastern.edu>
2024-11-22 23:07:20 -0500

    a5

---
+++ Kanbas/Enrollments/dao.js
@@ -0,0 +1,24 @@
+import Database from "../Database/index.js";
+
+export function enrollUserInCourse(userId, courseId) {
+    const { enrollments } = Database;
+    enrollments.push({ 
+        _id: new Date().getTime().toString(),
+        user: userId, 
+        course: courseId 
+    });
+}
+
+export function unenrollUserFromCourse(userId, courseId) {
+    const { enrollments } = Database;
+    Database.enrollments = enrollments.filter(
+        e => !(e.user === userId && e.course === courseId)
+    );
+}
+
+export function getUserEnrollments(userId) {
+    const { enrollments } = Database;
+    return enrollments.filter(
+        enrollment => enrollment.user === userId
+    );
+}
